4eb9bba74ed815eef9ecb9012cb38ed25c347221
akhmadrijalsyukri02mm1 <akhmadrijalsyukri02mm1@gmail.com>
2024-06-07 07:27:27 +0000

    add

--- javascript/tipeData.js
+++ javascript/tipeData.js
@@ -9,7 +9,25 @@ const dataSymbol = Symbol()
 
 // tipe data non primitif
 const dataArray = ["apel", "anggur", "pisang"]
+console.log(dataArray[1])
+dataArray[0] = "jeruk"
+dataArray.push("pir", "manggis")
+dataArray.unshift("mangga", "dukuh")
+console.log(dataArray)
+dataArray.pop()
+dataArray.shift()
+console.log(dataArray)
+
+
 const dataObject = {
     nama : "Akhmad Rijal Syukri",
     umur : 22,
+    nim : 2003033
 }
+dataObject.jenisKelamin = "Laki-laki"
+const dataNama = dataObject.nama
+console.log(dataNama)
+console.log(dataObject.umur)
+console.log(dataObject)
+delete dataObject.nim
+console.log(dataObject)
