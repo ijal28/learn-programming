34a32b211c8970ce3c18c05f43adac2988fbf2bf
akhmadrijalsyukri02mm1 <akhmadrijalsyukri02mm1@gmail.com>
2024-06-07 06:32:20 +0000

    add

---
+++ javascript/tipeData.js
@@ -0,0 +1,15 @@
+//tipe data primitif
+const dataString = "Akhmad Rijal Syukri"
+const dataNumber = 22
+const dataBoolean = true
+const dataNull = null
+let dataUndefined
+const dataBigInt = 9007199254740992n
+const dataSymbol = Symbol()
+
+// tipe data non primitif
+const dataArray = ["apel", "anggur", "pisang"]
+const dataObject = {
+    nama : "Akhmad Rijal Syukri",
+    umur : 22,
+}
