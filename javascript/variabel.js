f6f2785d53824f0834c9df77b4f240570a3191c0
akhmadrijalsyukri02mm1 <akhmadrijalsyukri02mm1@gmail.com>
2024-06-09 06:20:04 +0000

    push

--- javascript/variabel.js
+++ javascript/variabel.js
@@ -1,5 +1,20 @@
-let nama = "Akhmad Rijal Syukri"
-const umur = 21
 
-console.log(nama)
-console.log(umur)
+// Deklarasi variabel dengan let
+let nama = "Akhmad Rijal Syukri";
+
+// Deklarasi variabel dengan const
+const umur = 21;
+
+
+// Menampilkan nilai variabel
+console.log(nama);
+console.log(umur);
+console.log(alamat);
+
+// Mengubah nilai variabel let
+nama = "Rijal";
+console.log(nama);
+
+// Mengubah nilai variabel const (tidak dapat diubah)
+// umur = 22; // Error: Assignment to constant variable.
+
